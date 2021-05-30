ede353485cd408169180922580d95862804b505d
mohdAzzam <alazzamhmode@gmail.com>
2021-05-30 16:11:32 +0300

    prof of life aws

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # cloud-server 
 
+
+GUI - http://azzam-env.eba-rmwkdktf.us-east-1.elasticbeanstalk.com/
--- azzm-prof-of-life-source/server.js
+++ azzm-prof-of-life-source/server.js
@@ -2,9 +2,9 @@
 const express = require('express');
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+
 app.get('/',(req,res)=>{
     res.status(200).send('Welcome to Our Home Page');
 })
 
-app.listen(PORT);
+app.listen(3000);
